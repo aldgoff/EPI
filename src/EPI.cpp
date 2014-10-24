d994ca41294ab95673f649ef5181860302dad74a
Allan Goff <allangoff@gmail.com>
2014-10-23 21:35:45 -0700

    Copied over emailed correct version of EPI.cpp.

--- src/EPI.cpp
+++ src/EPI.cpp
@@ -2,14 +2,31 @@
 // Name        : EPI.cpp
 // Author      : Allan Goff
 // Version     :
-// Copyright   : Novatia Labs 2014
+// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include <cstdio>
+#include <cstring>
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!" << endl; // prints !!!Hello World!!!
+#include "meta.h"
+
+int main(int argc, char* argv[]) {
+	cout << "Hello EPI.\n" << endl;
+
+	cout << argv[0] << "\n\n";		// Where the executable is located.
+
+	if(argc == 1) {					// What commands EPI understands.
+		cout << "  No args.\n" << endl;
+		meta("");
+	}
+
+	for(int i=1; i<argc; i++)		// Process the args (requests).
+		meta(string(argv[i]));
+
+	cout << "Aloha EPI.\n" << endl;
+
 	return 0;
 }
