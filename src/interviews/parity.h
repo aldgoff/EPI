05b6c70d5a0d05596c311f7ca2db7ba341e1686d
aldgoff <aldgoff@ALDGOFF-MOBL1.amr.corp.intel.com>
2014-10-30 15:57:50 -0700

    Implement brute force strategy.

--- src/interviews/parity.h
+++ src/interviews/parity.h
@@ -20,24 +20,37 @@ namespace parity_compute {
 class Strategy {
 public:
 	virtual ~Strategy() {}
-	virtual void algorithm() {}
+	virtual unsigned algorithm(unsigned test) { return 0; }
 };
 class S1 : public Strategy {
 public:
-	void algorithm() { cout << "Strategy 1 of parity interview problem.\n"; }
+	unsigned algorithm(unsigned test) { // Brute force solution.
+		cout << "Strategy 1 of parity interview problem.\n";
+		short len = 8*sizeof(test);
+		unsigned char mask = 0x01;
+		short count = 0;
+		for(int i=0; i<len; i++) {
+			if((test >> i) & mask)
+				count++;
+		}
+		return count%2;
+	}
 };
 class S2 : public Strategy {
 public:
-	void algorithm() { cout << "Strategy 2 of parity interview problem.\n"; }
+	unsigned algorithm(unsigned test) {
+		cout << "Strategy 2 of parity interview problem.\n";
+		return 0;
+	}
 };
 
 void demo() {	// Run through 2D matrix: test cases versus strategies.
-	unsigned tests[] = { 1, 2 };
+	unsigned tests[] = { 1, 2, 7, 0xFF };
 	Strategy* poly[] = { new S1, new S2 };
 	for(size_t j=0; j<COUNT(tests); j++) {
-		cout << "\nData " << j+1 << endl;
+		cout << "\nData " << tests[j] << endl;
 		for(size_t i=0; i<COUNT(poly); i++) {
-			poly[i]->algorithm();
+			cout << poly[i]->algorithm(tests[j]) << endl;
 		}
 	}
 }
