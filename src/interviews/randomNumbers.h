173bd85be1acf853d5e8914dd27976934237ac31
aldgoff <aldgoff@ALDGOFF-MOBL1.amr.corp.intel.com>
2014-11-03 14:51:58 -0800

    Add random number range interview problem.

--- src/interviews/randomNumbers.h
+++ src/interviews/randomNumbers.h
@@ -14,6 +14,7 @@
 
 
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
@@ -31,15 +32,55 @@ public:
 class S1 : public Strategy {
 public:
 	int algorithm(int hi, int lo=0) {
-		cout << "Strategy 1 of random numbers interview problem.\n";
-		return 0;
-}
+		// Compute for next highest 2^n, repeat if outside range.
+		int rn = 0;
+		for(;;) {	// Best case, single pass, worse case probability fail 1/2 per pass.
+			int range = hi - lo;
+			rn = 0;
+			for(int i=0; range; i++) {
+				rn += ((rand()%2) << i);
+				range >>= 1;
+			}
+			if(rn <= hi - lo)
+				break;
+		}
+		return rn + lo;
+	}
 };
 class S2 : public Strategy {
 public:
 	int algorithm(int hi, int lo=0) {
-		cout << "Strategy 2 of random numbers interview problem.\n";
-		return 0;
+		// Compute for next highest 2^n, repeat if outside range.
+		int rn = 0;
+		for(int j=0; j<10; j++) {
+			int range = hi - lo;
+			rn = 0;
+			for(int i=0; range; i++) {
+				rn += ((rand()%2) << i);
+				range >>= 1;
+			}
+			if(rn <= hi - lo)
+				break;
+			if(j>0)
+				printf("(%d) ", j);
+		}
+		return rn + lo;
+	}
+};
+class S3 : public Strategy {
+public:
+	int algorithm(int hi, int lo=0) {
+		// Compute for next highest 2^n, repeat if outside range.
+		int rn = 0, t = hi - lo;
+		for(;;) {
+			rn = 0;
+			for(int i=0; (1<<i <= t); i++) {
+				rn = (rn << 1) | (rand()%2);
+			}
+			if(rn <= t)
+				break;
+		}
+		return rn + lo;
 	}
 };
 
@@ -50,13 +91,21 @@ struct Data {
 
 void demo() {	// Run through 2D matrix: test cases versus strategies.
 	Data tests[] = {
-		{ 5, 0 },
+		{ 1, 0 },
+		{ 3, 0 },
+		{ 7, 0 },
+		{ 9, 0 },
+		{ 9, 0 },
+		{ 9, 7 },
 	};
-	Strategy* poly[] = { new S1, new S2 };
+	Strategy* poly[] = { new S1, new S2, new S3 };
 	for(size_t j=0; j<COUNT(tests); j++) {
-		cout << "\nData " << j+1 << endl;
+		cout << "\nData " << tests[j].hi << " " << tests[j].lo << endl;
 		for(size_t i=0; i<COUNT(poly); i++) {
-			cout << poly[i]->algorithm(tests[j].hi, tests[j].lo) << endl;
+			for(int k=0; k<20; k++) {
+				cout << poly[i]->algorithm(tests[j].hi, tests[j].lo) << " ";
+			}
+			cout << endl;
 		}
 	}
 }
